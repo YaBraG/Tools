58e49268ce6af372217f92a55dd331509cdfaad2
Erick Licona <92254077+YaBraG@users.noreply.github.com>
2026-04-13 18:45:31 -0400

    Update metadata.py

--- app/metadata.py
+++ app/metadata.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 APP_NAME = "Tools"
-APP_VERSION = "0.1.2"
+APP_VERSION = "0.1.3"
 GITHUB_REPOSITORY = "YaBraG/Tools"
 GITHUB_RELEASES_URL = f"https://github.com/{GITHUB_REPOSITORY}/releases"
 GITHUB_LATEST_RELEASE_API_URL = (
